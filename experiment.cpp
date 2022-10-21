0f460596f7fa769eca0eaf3e938dd920f78cb211
Tales Prado <talesprado@gmail.com>
2022-10-21 11:08:20 -0300

    Update experiment.cpp

--- experiment.cpp
+++ experiment.cpp
@@ -2,42 +2,29 @@
 #include <ArduinoJson.h>
 
 Experiment xp("Poste de luz", "Simula o funcionamento automatico de um poste de luz");
+int variavel = 0;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   
-  StaticJsonDocument<200> doc;
-  
-   
-
-   xp.addVariable("var1", "valor1");
-   xp.addVariable("var2", "valor2");
+  xp.addVariable("var1", "Tem luz",   false);
+  xp.addVariable("var2", "Noite", false);
+  xp.addVariable("var3", "Chuva", true);
+  xp.addSensor(12, "Fotovoltaico", 60);
+  xp.updateVariable("var1", true);
 
   Serial.begin(9600);
-  while(!Serial) continue;
-  
-  //serializeJsonPretty(, Serial);
-  
+  while(!Serial) continue;  
+
 }
 
 void loop() {
-  StaticJsonDocument<100> com;
-  /*delay(1000);
-  digitalWrite(LED_BUILTIN, HIGH);
   
-  serializeJson(xp.getDoc(), Serial);
-  Serial.print("/EOJO");
-  delay(1000);
-  xp.updateVariable("re", "2000");
-  digitalWrite(LED_BUILTIN, LOW);*/
-  if (Serial.available()){
-    
-    deserializeJson(com, Serial);
-    if (com["action"] ==  "update"){
-      digitalWrite(LED_BUILTIN, HIGH);
-      serializeJson(xp.getDoc(), Serial);
-      Serial.print("/EOJO");
-    } 
-    
+  if (xp.getValue<bool>("var1") and ( xp.getValue<bool>("var2") or xp.getValue<bool>("var3"))){
+    digitalWrite(LED_BUILTIN, HIGH);
+  }else{
+    digitalWrite(LED_BUILTIN, LOW);
   }
+  xp.updateExperiment();
+   
 }
