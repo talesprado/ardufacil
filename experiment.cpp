3fc3c15a551809028aca79dc58a34a153569d94f
Tales Prado <talesprado@gmail.com>
2022-11-01 17:25:08 -0300

    Update experiment.cpp

--- experiment.cpp
+++ experiment.cpp
@@ -7,24 +7,25 @@ int variavel = 0;
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   
-  xp.addVariable("var1", "Tem luz",   false);
-  xp.addVariable("var2", "Noite", false);
-  xp.addVariable("var3", "Chuva", true);
-  xp.addSensor(12, "Fotovoltaico", 60);
-  xp.updateVariable("var1", true);
+  xp.addVariable("var1", "Tem luz",   true);
+  xp.addVariable("var2", "Noite", true);
+  xp.addVariable("var3", "Chuva", false);
+  xp.addSensor(123, "Fotovoltaico", 1);
 
   Serial.begin(9600);
   while(!Serial) continue;  
-
+  xp.readEepromToVariables(); 
+  
 }
 
 void loop() {
-  
-  if (xp.getValue<bool>("var1") and ( xp.getValue<bool>("var2") or xp.getValue<bool>("var3"))){
+  Serial.println("ta loopando");
+  xp.updateExperiment();
+/*  if (xp.getValue<bool>("var1") and ( xp.getValue<bool>("var2") or xp.getValue<bool>("var3"))){
     digitalWrite(LED_BUILTIN, HIGH);
   }else{
     digitalWrite(LED_BUILTIN, LOW);
-  }
-  xp.updateExperiment();
-   
+  }*/
+  
+   delay(1000);
 }
